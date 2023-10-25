1016a16d3deeb906c98459d6b0c1db497dc81904
kevster2017 <kevin.okane2017@gmail.com>
2023-10-25 11:48:30 +0100

    datable tables set up. isAdmin added to register

--- database/migrations/2023_10_25_104105_create_contacts_table.php
+++ database/migrations/2023_10_25_104105_create_contacts_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->boolean('isAdmin')->default(0);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('name');
+            $table->long('userID');
+            $table->string('email');
+            $table->string('message');
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
