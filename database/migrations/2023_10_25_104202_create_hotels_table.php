71144eef30ed0d5b38a600aaac1b6e3ed0328df8
kevster2017 <kevin.okane2017@gmail.com>
2023-10-25 19:50:05 +0100

    Hotel DB updated

--- database/migrations/2023_10_25_104202_create_hotels_table.php
+++ database/migrations/2023_10_25_104202_create_hotels_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('accomType');
             $table->string('roomType');
             $table->string('holidayType');
+            $table->string('hotelOptions');
             $table->string('currency');
             $table->integer('price');
             $table->integer('numRooms');
