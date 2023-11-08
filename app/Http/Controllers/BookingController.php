44fd04a1e383323b0d573dea0f6536bba22d71aa
kevster2017 <kevin.okane2017@gmail.com>
2023-11-08 21:15:13 +0000

    Booking functions added to booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -43,6 +43,121 @@ class BookingController extends Controller
         return view('bookings.paymentComplete');
     }
 
+    /**
+     * Book Hotel by adding to Cart
+     */
+
+    function addToCart(Request $request)
+    {
+
+
+        $cart = new Cart;
+        $cart->user_id = auth()->user()->id;
+        $cart->product_id = $request->product_id;
+        $cart->save();
+        return redirect('/products')->with('success', 'Item added to cart');
+    }
+
+    static function cartItem()
+    {
+
+        $user_id = optional(Auth::user())->id;
+
+        return Cart::where('user_id', $user_id)->count();
+    }
+
+    function cartList()
+    {
+
+        $user_id = auth()->user()->id;
+
+        $products = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $user_id)
+            ->select('products.*', 'cart.id as cart_id')
+            ->get();
+
+        return view('orders.cartList', ['products' => $products]);
+    }
+
+    function removeCart($id)
+    {
+        Cart::destroy($id);
+
+        return redirect()->back()->with('success', 'Item successfully removed from cart');
+    }
+
+    function bookNow()
+    {
+
+        $user_id = auth()->user()->id;
+        $total = DB::table('cart')
+            ->join('products', 'cart.product_id', '=', 'products.id')
+            ->where('cart.user_id', $user_id)
+            ->sum('products.price');
+
+
+        return view('orders.orderNow', ['total' => $total]);
+    }
+
+    function placeBooking(Request $req)
+    {
+        $user_id = auth()->user()->id;
+        $fullCart = Cart::where('user_id', $user_id)
+            ->get();
+
+        $req->validate([
+            'address' => 'required',
+            'payment' => 'required'
+        ]);
+
+        foreach ($fullCart as $cart) {
+            $order = new Order;
+            $order->product_id = $cart->product_id;
+            $order->user_id = $cart->user_id;
+            $order->name = $req->name;
+            $order->status = "Pending";
+            $order->address = $req->address;
+            $order->payment_method = $req->payment;
+            $order->payment_status = "Pending";
+            $order->save();
+            // Cart::where('user_id', $user_id)->delete();
+
+        }
+
+        if ($order->payment_method == 'Online') {
+            return redirect('/orders/stripe');
+        }
+
+        if ($order->payment_method == 'PayPal') {
+            $total = DB::table('cart')
+                ->join('products', 'cart.product_id', '=', 'products.id')
+                ->where('cart.user_id', $user_id)
+                ->sum('products.price');
+
+            return view('/orders/paypal', ['total' => $total]);
+        } else {
+            return redirect('/orders/stripe');
+        }
+    }
+
+    function myBookings()
+    {
+
+        $user_id = auth()->user()->id;
+
+        $ids = DB::table('orders')
+            ->where('user_id', $user_id)
+            ->get();
+
+        $orders = DB::table('orders')
+            ->join('products', 'orders.product_id', '=', 'products.id')
+            ->where('orders.user_id', $user_id)
+            ->select('*', 'orders.id as order_id') // Selects the original order ID
+            ->paginate(5);
+
+        return view('orders.myOrders', ['orders' => $orders, 'ids' => $ids]);
+    }
 
     /**
      * Store a newly created resource in storage.
