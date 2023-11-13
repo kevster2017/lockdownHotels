033b0abaa83e7f1379027051ecc34b27020b91c8
kevster2017 <kevin.okane2017@gmail.com>
2023-11-13 19:49:04 +0000

    myBookings blade updated

--- resources/views/bookings/myBookings.blade.php
+++ resources/views/bookings/myBookings.blade.php
@@ -5,40 +5,40 @@
 
 
 
-<h1 class="text-center pb-3">My Orders</h1>
+<h1 class="text-center pb-3">My Bookings</h1>
 
 <div class="container mt-5 d-flex justify-content-center">
   <table class="table table-striped table-bordered table-hover">
     <thead>
       <tr>
-        <th scope="col">Order ID</th>
+        <th scope="col">Booking ID</th>
         <th scope="col">Product</th>
         <th scope="col">Title</th>
         <th scope="col">Address</th>
         <th scope="col">Payment Method</th>
         <th scope="col">Payment Status</th>
         <th scope="col">Status</th>
-        <th scope="col">Order Placed</th>
-        <th scope="col">Order Updated</th>
+        <th scope="col">Booking Placed</th>
+        <th scope="col">Booking Updated</th>
 
       </tr>
     </thead>
     <tbody>
 
 
-      @foreach($orders as $order)
+      @foreach($bookings as $booking)
       <tr>
 
-        <th scope="row">{{ $order->order_id }}</a></th>
+        <th scope="row">{{ $booking->booking_id }}</a></th>
 
-        <td><img class="trending-img" src="/storage/{{ $order->image }}"></td>
-        <td>{{ $order->name }}</td>
-        <td>{{ $order->address }}</td>
-        <td>{{ $order->payment_method }}</td>
-        <td>{{ $order->payment_status }}</td>
-        <td>{{ $order->status }}</td>
-        <td>{{ date('d-m-Y', strtotime($order->created_at));  }}</td>
-        <td>{{ date('d-m-Y', strtotime($order->updated_at)); }}</td>
+        <td><img class="trending-img" src="/storage/{{ $booking->image }}"></td>
+        <td>{{ $booking->name }}</td>
+        <td>{{ $booking->address }}</td>
+        <td>{{ $booking->payment_method }}</td>
+        <td>{{ $booking->payment_status }}</td>
+        <td>{{ $booking->status }}</td>
+        <td>{{ date('d-m-Y', strtotime($booking->created_at));  }}</td>
+        <td>{{ date('d-m-Y', strtotime($booking->updated_at)); }}</td>
 
 
       </tr>
@@ -53,7 +53,7 @@
 
 </div>
 <div class="pagination justify-content-center">
-  {{ $orders->links() }}
+  {{ $bookings->links() }}
 </div>
 
 @endsection
