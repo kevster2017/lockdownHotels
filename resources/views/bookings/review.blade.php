4705975fbdbb47c3f064d9fb1183a94b74f08b49
kevster2017 <kevin.okane2017@gmail.com>
2023-11-17 10:41:39 +0000

    review blade updated

--- resources/views/bookings/review.blade.php
+++ resources/views/bookings/review.blade.php
@@ -10,7 +10,11 @@
 </div>
 
 <div class="container">
-    <div class="card mb-3" style="max-width: 540px;">
+    <h1>Review Booking</h1>
+</div>
+
+<div class="container mt-3">
+    <div class="card mb-3" style="max-width: 1080px;">
         <div class="row g-0">
             <div class="col-md-4">
 
@@ -21,13 +25,94 @@
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">{{ $cart->name }}</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title" id="divLeft">{{ $cart->name }}</h5>
+                    <strong><label id="divLeft">Address</label></strong>
+                    <p class="card-text" id="divLeft">{{ $cart->address }}</p>
+                    <p class="card-text" id="divLeft">{{ $cart->town }}</p>
+                    <p class="card-text" id="divLeft">{{ $cart->country }}</p>
+                    <p class="card-text" id="divLeft">{{ $cart->postCode }}</p>
 
                     <p class="card-text"><small class="text-body-secondary">Added to Cart: {{ $cart->created_at }}</small></p>
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+<div class="container mt-3">
+
+    <div class="card">
+        <h5 class="card-header" id="divLeft">Booking Details</h5>
+        <div class="card-body">
+            <p class="card-text" id="divLeft"> {{ $cart->roomType }} room</p>
+            <p class="card-text" id="divLeft">Check in: {{ $cart->checkInDate }}</p>
+            <p class="card-text" id="divLeft"> {{ $cart->numNights }} nights</p>
+            <p class="card-text" id="divLeft"><strong>Total Price: £{{ $cart->price }}</strong></p>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-3">
+
+    <div class="card">
+        <h5 class="card-header" id="divLeft">Extras Details</h5>
+        <div class="card-body">
+            @if($cart->feat1 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->feat1 }}</p>
+            @endif
+            @if($cart->feat2 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->feat2 }}</p>
+            @endif
+            @if($cart->feat3 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->feat3 }}</p>
+            @endif
+            @if($cart->feat4 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->feat4 }}</p>
+            @endif
+            <p class="card-text" id="divLeft"><strong>Total Price: £{{ $cart->price }}</strong></p>
+        </div>
+    </div>
+
+    <div class="card mt-3">
+        <h5 class="card-header" id="divLeft">Upgrade Details</h5>
+        <div class="card-body">
+            @if($cart->upgrade1 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->upgrade1 }}</p>
+            @endif
+            @if($cart->upgrade2 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->upgrade2 }}</p>
+            @endif
+            @if($cart->upgrade3 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->upgrade3 }}</p>
+            @endif
+            @if($cart->upgrade1 == 0 && $cart->upgrade2 == 0 && $cart->upgrade3 == 0)
+            <p class="card-text" id="divLeft">No upgrades selected</p>
+            @endif
+            <p class="card-text" id="divLeft"><strong>Total Price: £{{ $cart->price }}</strong></p>
+        </div>
+    </div>
+
+    <div class="card mt-3">
+        <h5 class="card-header" id="divLeft">Package Details</h5>
+        <div class="card-body">
+            @if($cart->package1 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->package1 }}</p>
+            @endif
+            @if($cart->package2 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->package2 }}</p>
+            @endif
+            @if($cart->package3 != 0)
+            <p class="card-text" id="divLeft">Feature: {{ $cart->package3 }}</p>
+            @endif
+            @if($cart->package1 == 0 && $cart->package2 == 0 && $cart->package3 == 0)
+            <p class="card-text" id="divLeft">No packages selected</p>
+            @endif
+            <p class="card-text" id="divLeft"><strong>Total Price: £{{ $cart->price }}</strong></p>
+        </div>
+    </div>
+
+    <div class="container">
+        <a href="#" class="btn btn-primary">Book Now for £{{ $cart->price}}</a>
     </div>
 </div>
 @endsection
