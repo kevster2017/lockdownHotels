b78ca6c37d349fe360680e1ff8543bdfcd8202a8
kevster2017 <kevin.okane2017@gmail.com>
2023-11-20 19:56:40 +0000

    show blade updated

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -47,6 +47,55 @@
     </div>
 </div>
 
+<div class="container mt-3">
+
+    <div class="card">
+        <h5 class="card-header" id="divLeft">Cost Breakdown</h5>
+        <div class="card-body">
+
+
+
+            <div class="row">
+
+                <div class="col-sm-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header bg-transparent border-dark">Hotel Costs</div>
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">Price Per Night: £{{ $booking->pricePN }}</h5>
+
+                        </div>
+                        <div class="card-footer bg-transparent border-dark"><strong>Total Hotel Costs:</strong> £{{ $booking->pricePN * $booking->numNights }}</div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header bg-transparent border-dark">Extras Costs</div>
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">Success card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer bg-transparent border-dark"><strong>Total Extras Costs:</strong> £{{ $booking->extrasTotal }}</div>
+                    </div>
+                </div>
+
+                <div class="col-sm-4">
+                    <div class="card border-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header bg-transparent border-dark">Total Costs</div>
+                        <div class="card-body text-dark">
+                            <h5 class="card-title">Success card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                        <div class="card-footer bg-transparent border-dark"><strong>Total Costs:</strong> £{{ $booking->total }}</div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
 
 
 @endsection
