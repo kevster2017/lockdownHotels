6ef74adc3a19fb60caa7478a3dc700488c2ac705
kevster2017 <kevin.okane2017@gmail.com>
2023-11-22 12:32:34 +0000

    seaside and city index blades updated

--- resources/views/hotels/cityIndex.blade.php
+++ resources/views/hotels/cityIndex.blade.php
@@ -6,7 +6,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">City Break hotels</li>
+            <li class="breadcrumb-item active" aria-current="page">City Break Hotels</li>
         </ol>
     </nav>
 </div>
@@ -14,45 +14,32 @@
 <div class="container py-3">
 
 
-    <h1 class="text-center py-3">City Break hotels</h1>
+    <h1 class="text-center py-3">City Break Hotels</h1>
 
+    <div class="container d-flex justify-content-center mt-3">
+        @foreach($hotels as $hotel)
 
-    @foreach($hotels as $hotel)
-    <div class="row">
-        <div class="col-sm-8 mx-auto">
+        <div class="card mb-3">
+            <div class="row g-0">
+                <div class="col-sm-4">
+                    <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}} " class="img-responsive rounded-start " alt="Hotel Image"></a>
 
-            <!-- List group-->
-            <ul class="list-group" id="indexCard">
+                </div>
+                <div class="col-sm-8">
+                    <div class="card-body divLeft">
+                        <a href="{{ route('hotels.show', $hotel->id) }}">
+                            <h5 class="card-title">Name: {{ $hotel->name}}</h5>
+                        </a>
 
-                <!-- list group item-->
-                <li class="list-group-item">
+                        <p class="card-text">Description: {{ $hotel->description }}</p>
+                        <p class="card-text">Home Town: {{ $hotel->town }}</p>
+                        <p class="card-text"><small class="text-muted">Hotel Added: {{ $hotel->created_at-> diffforhumans() }}</small></p>
 
-                    <div class="my-2" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col">
-                                <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}} " class="img-responsive rounded-start" alt="Hotel Image" id="indexImage"></a>
-                            </div>
-                            <div class="col ms-3 pt-3">
-                                <div class="card-body">
-                                    <a href="{{ route('hotels.show', $hotel->id) }}">
-                                        <h5 class="card-title">Name: {{ $hotel->name}}</h5>
-                                    </a>
-
-                                    <p class="card-text">Description: {{ $hotel->description }}</p>
-                                    <p class="card-text">Home Town: {{ $hotel->town }}</p>
-                                    <p class="card-text"><small class="text-muted">Reported hotel: {{ $hotel->created_at-> diffforhumans() }}</small></p>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-
-
-                    <!-- End -->
-                </li>
-                <!-- End -->
-            </ul>
+                </div>
+            </div>
         </div>
+
+        @endforeach
     </div>
-    <br>
-    @endforeach
     @endsection
--- resources/views/hotels/seasideIndex.blade.php
+++ resources/views/hotels/seasideIndex.blade.php
@@ -2,57 +2,38 @@
 
 @section('content')
 <!-- Page Content -->
-<div class="container">
+<div class="container text-center mt-3">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Seaside hotels</li>
+            <li class="breadcrumb-item active" aria-current="page">Seaside Hotels</li>
         </ol>
     </nav>
+
+    <h1 class="py-3">Seaside Hotels</h1>
 </div>
 
-<div class="container py-3">
+@foreach($hotels as $hotel)
+<div class="container d-flex justify-content-center mt-3">
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-sm-4">
+                <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}} " class="img-responsive rounded-start " alt="Hotel Image"></a>
+            </div>
+            <div class="col-sm-8">
+                <div class="card-body">
+                    <a href="{{ route('hotels.show', $hotel->id) }}">
+                        <h5 class="card-title">Name: {{ $hotel->name}}</h5>
+                    </a>
 
-
-    <h1 class="text-center py-3">Seaside Hotels</h1>
-
-
-    @foreach($hotels as $hotel)
-    <div class="row">
-        <div class="col-sm-8 mx-auto">
-
-            <!-- List group-->
-            <ul class="list-group" id="indexCard">
-
-                <!-- list group item-->
-                <li class="list-group-item">
-
-                    <div class="my-2" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col">
-                                <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}} " class="img-responsive rounded-start" alt="Hotel Image" id="indexImage"></a>
-                            </div>
-                            <div class="col ms-3 pt-3">
-                                <div class="card-body">
-                                    <a href="{{ route('hotels.show', $hotel->id) }}">
-                                        <h5 class="card-title">Name: {{ $hotel->name}}</h5>
-                                    </a>
-
-                                    <p class="card-text">Description: {{ $hotel->description }}</p>
-                                    <p class="card-text">Home Town: {{ $hotel->town }}</p>
-                                    <p class="card-text"><small class="text-muted">Reported hotel: {{ $hotel->created_at-> diffforhumans() }}</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <!-- End -->
-                </li>
-                <!-- End -->
-            </ul>
+                    <p class="card-text">Description: {{ $hotel->description }}</p>
+                    <p class="card-text">Home Town: {{ $hotel->town }}</p>
+                    <p class="card-text"><small class="text-muted">Hotel Added: {{ $hotel->created_at-> diffforhumans() }}</small></p>
+                </div>
+            </div>
         </div>
     </div>
-    <br>
-    @endforeach
-    @endsection
+</div>
+@endforeach
+
+@endsection
