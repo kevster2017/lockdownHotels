53ddc3f875e94b83c4163270509cbd624403f1b7
kevster2017 <kevin.okane2017@gmail.com>
2023-11-22 14:41:40 +0000

    seaside index blade updated

--- resources/views/hotels/seasideIndex.blade.php
+++ resources/views/hotels/seasideIndex.blade.php
@@ -15,25 +15,27 @@
 
 @foreach($hotels as $hotel)
 <div class="container d-flex justify-content-center mt-3">
-    <div class="card mb-3">
+    <div class="card w-75">
         <div class="row g-0">
-            <div class="col-sm-4">
-                <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}} " class="img-responsive rounded-start " alt="Hotel Image"></a>
+            <div class="col-12 col-sm-4 order-sm-1">
+                <a href="{{ route('hotels.show', $hotel->id) }}"><img src="/storage/{{$hotel->image}}" class="img-responsive rounded-start img-fluid" alt="Hotel Image"></a>
             </div>
-            <div class="col-sm-8">
-                <div class="card-body">
+            <div class="col-12 col-sm-8 order-sm-2">
+                <div class="card-body divLeft">
                     <a href="{{ route('hotels.show', $hotel->id) }}">
-                        <h5 class="card-title">Name: {{ $hotel->name}}</h5>
+                        <h5 class="card-title">{{ $hotel->name}}</h5>
                     </a>
 
-                    <p class="card-text">Description: {{ $hotel->description }}</p>
-                    <p class="card-text">Home Town: {{ $hotel->town }}</p>
-                    <p class="card-text"><small class="text-muted">Hotel Added: {{ $hotel->created_at-> diffforhumans() }}</small></p>
+                    <p class="card-text">Room Type: {{ $hotel->roomType}}</p>
+                    <p class="card-text">Town: {{ $hotel->town }}</p>
+                    <p class="card-text"><small class="text-muted">Hotel Added: {{ $hotel->created_at->diffForHumans() }}</small></p>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endforeach
+
+
 
 @endsection
