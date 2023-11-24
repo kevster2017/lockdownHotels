3532341ddcde059514e61d75f019bad74723236a
kevster2017 <kevin.okane2017@gmail.com>
2023-11-24 18:10:24 +0000

    welcome blade and style.css updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div class="container-fluid">
+<div class="container-fluid" id="welcomeContainer">
     <div class="row">
         <div class="col-sm-6 my-auto">
             <strong>
@@ -12,13 +12,13 @@
             <p class="text-center">Want to find the perfect getaway? Come join us and find your dream hotel</p>
             <div class="row d-flex justify-content-center">
                 <a href="{{ route('login') }}" class=" btn btn-primary">Login</a>
-                <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
+                <a href="{{ route('register') }}" class="btn btn-info">Register</a>
 
             </div>
 
         </div>
         <div class="col-sm-6">
-            <img src="/images/HotelsOpaque.PNG">
+            <img class="img-thumbnail" src="/images/HotelsOpaque.PNG" alt="Logo">
         </div>
     </div>
 </div>
