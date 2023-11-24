7bb518982cfa179031e4ce0ee6bdeb38bff906d5
kevster2017 <kevin.okane2017@gmail.com>
2023-11-24 18:32:10 +0000

    welcome blade updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,22 @@
 
 
 <div class="container-fluid" id="welcomeContainer">
-    <div class="row">
-        <div class="col-sm-6 my-auto">
+    <div class="row d-flex align-items-center">
+        <div class="col-sm-6">
             <strong>
                 <h1 class="text-center">Welcome to Lockdown Hotels</h1>
             </strong>
             <p class="text-center">Want to find the perfect getaway? Come join us and find your dream hotel</p>
             <div class="row d-flex justify-content-center">
-                <a href="{{ route('login') }}" class=" btn btn-primary">Login</a>
+                <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
                 <a href="{{ route('register') }}" class="btn btn-info">Register</a>
-
             </div>
-
         </div>
         <div class="col-sm-6">
             <img class="img-thumbnail" src="/images/HotelsOpaque.PNG" alt="Logo">
         </div>
     </div>
 </div>
-
 
 
 @endsection
