63bccaeb43cf6bc2edfe6ea6ecb9f6f377ef7a22
kevster2017 <kevin.okane2017@gmail.com>
2023-12-06 19:00:53 +0000

    stripe controller updated

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -44,50 +44,54 @@ class StripeController extends Controller
         /* Create a new Booking */
         $booking = new Booking;
 
-$booking->userId = $cart->userId;
-$booking->name = $cart->name;
-$booking->hotelId = $cart->hotelId;
-$booking->image = $cart->image;
-$booking->address = $cart->address;
-$booking->postCode = $cart->postCode;
-$booking->accomType = $cart->accomType;
-$booking->roomType = $cart->roomType;
-$booking->holidayType = $cart->holidayType;
-$booking->price = $cart->price;
-$booking->checkInDate = $cart->checkInDate;
-$booking->numNights = $cart->numNights;
-$booking->feat1 = $cart->feat1;
-$booking->feat2 = $cart->feat2;
-$booking->feat3 = $cart->feat3;
-$booking->feat4 = $cart->feat4;
-$booking->feat1Price = $cart->feat1Price;
-$booking->feat2Price = $cart->feat2Price;
-$booking->feat3Price = $cart->feat3Price;
-$booking->feat4Price = $cart->feat4Price;
-$booking->selectedFeat1 = $cart->selectedFeat1;
-$booking->selectedFeat2 = $cart->selectedFeat2;
-$booking->selectedFeat3 = $cart->selectedFeat3;
-$booking->selectedFeat4 = $cart->selectedFeat4;
-$booking->featuresTotal = $cart->featuresTotal;
-$booking->upgrade1 = $cart->upgrade1;
-$booking->upgrade2 = $cart->upgrade2;
-$booking->upgrade3 = $cart->upgrade3;
-$booking->upgrade1Price = $cart->upgrade1Price;
-$booking->upgrade2Price = $cart->upgrade2Price;
-$booking->upgrade3Price = $cart->upgrade3Price;
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
-$booking-> = $cart->
+        $booking->userId = $cart->userId;
+        $booking->name = $cart->name;
+        $booking->hotelId = $cart->hotelId;
+        $booking->image = $cart->image;
+        $booking->address = $cart->address;
+        $booking->postCode = $cart->postCode;
+        $booking->accomType = $cart->accomType;
+        $booking->roomType = $cart->roomType;
+        $booking->holidayType = $cart->holidayType;
+        $booking->price = $cart->price;
+        $booking->checkInDate = $cart->checkInDate;
+        $booking->numNights = $cart->numNights;
+        $booking->feat1 = $cart->feat1;
+        $booking->feat2 = $cart->feat2;
+        $booking->feat3 = $cart->feat3;
+        $booking->feat4 = $cart->feat4;
+        $booking->feat1Price = $cart->feat1Price;
+        $booking->feat2Price = $cart->feat2Price;
+        $booking->feat3Price = $cart->feat3Price;
+        $booking->feat4Price = $cart->feat4Price;
+        $booking->selectedFeat1 = $cart->selectedFeat1;
+        $booking->selectedFeat2 = $cart->selectedFeat2;
+        $booking->selectedFeat3 = $cart->selectedFeat3;
+        $booking->selectedFeat4 = $cart->selectedFeat4;
+        $booking->featuresTotal = $cart->featuresTotal;
+        $booking->upgrade1 = $cart->upgrade1;
+        $booking->upgrade2 = $cart->upgrade2;
+        $booking->upgrade3 = $cart->upgrade3;
+        $booking->upgrade1Price = $cart->upgrade1Price;
+        $booking->upgrade2Price = $cart->upgrade2Price;
+        $booking->upgrade3Price = $cart->upgrade3Price;
+        $booking->selectedUpgrade = $cart->selectedUpgrade;
+        $booking->upgradeTotal = $cart->upgradeTotal;
+        $booking->package1 = $cart->package1;
+        $booking->package2 = $cart->package2;
+        $booking->package3 = $cart->package3;
+        $booking->package1Price = $cart->package1Price;
+        $booking->package2Price = $cart->package2Price;
+        $booking->package3Price = $cart->package3Price;
+        $booking->selectedPackage = $cart->selectedPackage;
+        $booking->packageTotal = $cart->packageTotal;
+        $booking->currency = $cart->currency;
+        $booking->extrasTotal = $cart->extrasTotal;
+        $booking->finalTotal = $cart->finalTotal;
+        $booking->paid = 1;
 
 
+        dd($booking);
 
         $hotel = $cart->name;
 
@@ -105,8 +109,8 @@ $booking-> = $cart->
 
 
 
-        $bookings->paid = 1;
-        $bookings->save();
+
+        $booking->save();
 
 
 
