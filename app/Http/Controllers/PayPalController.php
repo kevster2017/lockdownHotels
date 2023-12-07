0910b8589a20b84de95ba87107d6d1f87322a913
kevster2017 <kevin.okane2017@gmail.com>
2023-12-07 17:27:37 +0000

    $bookings added to paymentSuccess, PayPal controll

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -46,59 +46,7 @@ class PayPalController extends Controller
             return redirect()->back()->with('error', 'Cart not found');
         }
 
-        /* Create a new Booking */
-        $bookings = new Booking;
 
-        $bookings->userId = $cart->userId;
-        $bookings->name = $cart->name;
-        $bookings->hotel_Id = $cart->hotel_Id;
-        $bookings->image = $cart->image;
-        $bookings->address = $cart->address;
-        $bookings->town = $cart->town;
-        $bookings->postCode = $cart->postCode;
-        $bookings->country = $cart->country;
-        $bookings->accomType = $cart->accomType;
-        $bookings->roomType = $cart->roomType;
-        $bookings->holidayType = $cart->holidayType;
-        $bookings->price = $cart->price;
-        $bookings->checkInDate = $cart->checkInDate;
-        $bookings->numNights = $cart->numNights;
-        $bookings->feat1 = $cart->feat1;
-        $bookings->feat2 = $cart->feat2;
-        $bookings->feat3 = $cart->feat3;
-        $bookings->feat4 = $cart->feat4;
-        $bookings->feat1Price = $cart->feat1Price;
-        $bookings->feat2Price = $cart->feat2Price;
-        $bookings->feat3Price = $cart->feat3Price;
-        $bookings->feat4Price = $cart->feat4Price;
-        $bookings->selectedFeat1 = $cart->selectedFeat1;
-        $bookings->selectedFeat2 = $cart->selectedFeat2;
-        $bookings->selectedFeat3 = $cart->selectedFeat3;
-        $bookings->selectedFeat4 = $cart->selectedFeat4;
-        $bookings->featuresTotal = $cart->featuresTotal;
-        $bookings->upgrade1 = $cart->upgrade1;
-        $bookings->upgrade2 = $cart->upgrade2;
-        $bookings->upgrade3 = $cart->upgrade3;
-        $bookings->upgrade1Price = $cart->upgrade1Price;
-        $bookings->upgrade2Price = $cart->upgrade2Price;
-        $bookings->upgrade3Price = $cart->upgrade3Price;
-        $bookings->selectedUpgrade = $cart->selectedUpgrade;
-        $bookings->upgradeTotal = $cart->upgradeTotal;
-        $bookings->package1 = $cart->package1;
-        $bookings->package2 = $cart->package2;
-        $bookings->package3 = $cart->package3;
-        $bookings->package1Price = $cart->package1Price;
-        $bookings->package2Price = $cart->package2Price;
-        $bookings->package3Price = $cart->package3Price;
-        $bookings->selectedPackage = $cart->selectedPackage;
-        $bookings->packageTotal = $cart->packageTotal;
-        $bookings->currency = $cart->currency;
-        $bookings->extrasTotal = $cart->extrasTotal;
-        $bookings->finalTotal = $cart->finalTotal;
-        $bookings->payment_method = 'PayPal';
-        $bookings->paid = 1;
-
-        dd($bookings);
 
         $provider = new PayPalClient;
         $provider->setApiCredentials(config('paypal'));
@@ -136,8 +84,6 @@ class PayPalController extends Controller
                 ->route('create.payment')
                 ->with('error', $response['message'] ?? 'Something went wrong.');
         }
-
-        /* If payment successful, create the booking */
     }
 
     /**
@@ -165,9 +111,73 @@ class PayPalController extends Controller
         $response = $provider->capturePaymentOrder($request['token']);
 
         if (isset($response['status']) && $response['status'] == 'COMPLETED') {
+            /* If payment successful, create the booking */
+            /* Create a new Booking */
+
+            $userId = auth()->user()->id;
+
+            $cart = Cart::where('userId', $userId)
+                ->first();
+            $bookings = new Booking;
+
+            $bookings->userId = $cart->userId;
+            $bookings->name = $cart->name;
+            $bookings->hotel_Id = $cart->hotel_Id;
+            $bookings->image = $cart->image;
+            $bookings->address = $cart->address;
+            $bookings->town = $cart->town;
+            $bookings->postCode = $cart->postCode;
+            $bookings->country = $cart->country;
+            $bookings->accomType = $cart->accomType;
+            $bookings->roomType = $cart->roomType;
+            $bookings->holidayType = $cart->holidayType;
+            $bookings->price = $cart->price;
+            $bookings->checkInDate = $cart->checkInDate;
+            $bookings->numNights = $cart->numNights;
+            $bookings->feat1 = $cart->feat1;
+            $bookings->feat2 = $cart->feat2;
+            $bookings->feat3 = $cart->feat3;
+            $bookings->feat4 = $cart->feat4;
+            $bookings->feat1Price = $cart->feat1Price;
+            $bookings->feat2Price = $cart->feat2Price;
+            $bookings->feat3Price = $cart->feat3Price;
+            $bookings->feat4Price = $cart->feat4Price;
+            $bookings->selectedFeat1 = $cart->selectedFeat1;
+            $bookings->selectedFeat2 = $cart->selectedFeat2;
+            $bookings->selectedFeat3 = $cart->selectedFeat3;
+            $bookings->selectedFeat4 = $cart->selectedFeat4;
+            $bookings->featuresTotal = $cart->featuresTotal;
+            $bookings->upgrade1 = $cart->upgrade1;
+            $bookings->upgrade2 = $cart->upgrade2;
+            $bookings->upgrade3 = $cart->upgrade3;
+            $bookings->upgrade1Price = $cart->upgrade1Price;
+            $bookings->upgrade2Price = $cart->upgrade2Price;
+            $bookings->upgrade3Price = $cart->upgrade3Price;
+            $bookings->selectedUpgrade = $cart->selectedUpgrade;
+            $bookings->upgradeTotal = $cart->upgradeTotal;
+            $bookings->package1 = $cart->package1;
+            $bookings->package2 = $cart->package2;
+            $bookings->package3 = $cart->package3;
+            $bookings->package1Price = $cart->package1Price;
+            $bookings->package2Price = $cart->package2Price;
+            $bookings->package3Price = $cart->package3Price;
+            $bookings->selectedPackage = $cart->selectedPackage;
+            $bookings->packageTotal = $cart->packageTotal;
+            $bookings->currency = $cart->currency;
+            $bookings->extrasTotal = $cart->extrasTotal;
+            $bookings->finalTotal = $cart->finalTotal;
+            $bookings->payment_method = 'PayPal';
+            $bookings->paid = 1;
+
+
+            $bookings->save();
+
+
+
+            Cart::where('userId', $userId)->delete();
+
             return redirect()
-                ->route('home')
-                ->with('success', 'Transaction complete.');
+                ->view('/bookings/myBookings')->with('success', 'Booking completed');
         } else {
             return redirect()
                 ->route('paypal')
