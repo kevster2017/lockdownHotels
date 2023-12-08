3a2ccda21df542c3b7ec5e1c90923aef67b77bcd
kevster2017 <kevin.okane2017@gmail.com>
2023-12-08 20:27:42 +0000

    delete modal added to bookings.show blade

--- resources/views/bookings/show.blade.php
+++ resources/views/bookings/show.blade.php
@@ -85,19 +85,19 @@
                             <h5 class="card-title">No Extras Purchased </h5>
                             @else
                             @if($booking->selectedFeat1 != 'None')
-                            <h5 class="card-title">{{ $booking->feat1 }}: {{$booking->feat1Price * $booking->numNights }} </h5>
+                            <h5 class="card-title">{{ $booking->feat1 }}: £{{$booking->feat1Price * $booking->numNights }} </h5>
 
                             @endif
                             @if($booking-> selectedFeat2 != 'None')
-                            <h5 class="card-title">{{ $booking->feat2 }}: {{$booking->feat2Price * $booking->numNights }} </h5>
+                            <h5 class="card-title">{{ $booking->feat2 }}: £{{$booking->feat2Price * $booking->numNights }} </h5>
 
                             @endif
                             @if($booking-> selectedFeat3 != 'None')
-                            <h5 class="card-title">{{ $booking->feat3 }}: {{$booking->feat3Price * $booking->numNights }} </h5>
+                            <h5 class="card-title">{{ $booking->feat3 }}: £{{$booking->feat3Price * $booking->numNights }} </h5>
 
                             @endif
                             @if($booking-> selectedFeat4 != 'None')
-                            <h5 class="card-title">{{ $booking->feat4 }}: {{$booking->feat4Price * $booking->numNights }} </h5>
+                            <h5 class="card-title">{{ $booking->feat4 }}: £{{$booking->feat4Price * $booking->numNights }} </h5>
 
                             @endif
                             @if($booking-> selectedUpgrade != 'None')
@@ -137,7 +137,33 @@
 
     <div class="row d-flex justify-content-center">
         <a class="btn btn-primary" href="{{ route('bookings.edit', $booking->id) }}">Edit Booking</a>
-        <a class="btn btn-danger" href="{{ route('bookings.destroy', $booking->id) }}">Delete Booking</a>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $booking->id }}">
+            Delete Booking
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="deleteModal{{ $booking->id }}" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteModalLabel">Are you sure you want to delete this booking?</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Deleting is permanent and cannot be undone</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <form method="POST" action="{{ route('bookings.destroy', $booking->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger" type="submit">Delete Booking</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 </div>
