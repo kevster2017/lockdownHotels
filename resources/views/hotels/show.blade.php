f6f74265211ccb8f97e978b3ba549cdda14488cb
kevster2017 <kevin.okane2017@gmail.com>
2023-12-10 21:30:12 +0000

    hotelIndex blade updated

--- resources/views/hotels/show.blade.php
+++ resources/views/hotels/show.blade.php
@@ -5,8 +5,8 @@
 <div class="container">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="/">Home</a></li>
-            <li class="breadcrumb-item"><a href="/">Hotels</a></li>
+            <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item"><a href="#" onclick="goBack()">Hotels</a></li>
             <li class="breadcrumb-item active" aria-current="page">{{ $hotel->name }}</li>
         </ol>
     </nav>
@@ -240,5 +240,9 @@
     }
 </script>
 
-
+<script>
+    function goBack() {
+        window.history.back();
+    }
+</script>
 @endsection
