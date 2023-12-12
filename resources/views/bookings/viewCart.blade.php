e5ee51fd4d33fe9d5d75e9d38a9b9c1b051a01ad
kevster2017 <kevin.okane2017@gmail.com>
2023-12-12 17:09:47 +0000

    viewCart blade buttons updated

--- resources/views/bookings/viewCart.blade.php
+++ resources/views/bookings/viewCart.blade.php
@@ -189,7 +189,7 @@
       <h2>Your total extras cost: £<span id="extrasCost">0</span></h2>
       <h2>Your total costs: £<span id="totalCost">0</span></span></h2>
       <!-- Button trigger modal -->
-      <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $cart->id }}">
+      <button type="button" class="btn btn-danger my-3 me-2" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $cart->id }}">
         Cancel Booking
       </button>
 
