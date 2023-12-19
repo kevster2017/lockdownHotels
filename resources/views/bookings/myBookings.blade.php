3bf5228ac6b697b9bc90f8bdd45e81b06ea2f96e
kevster2017 <kevin.okane2017@gmail.com>
2023-12-19 20:10:16 +0000

    style.css and myBookings blade updated

--- resources/views/bookings/myBookings.blade.php
+++ resources/views/bookings/myBookings.blade.php
@@ -26,35 +26,36 @@
     <div class="col-sm-8 mx-auto">
 
       <!-- List group-->
-      <ul class="list-group" id="indexCard">
+      <ul class="list-group">
+        <div class="cardStyle">
+          <!-- list group item-->
+          <li class="list-group-item">
 
-        <!-- list group item-->
-        <li class="list-group-item">
-
-          <div class="my-2">
-            <div class="row g-0">
-              <div class="col-sm-6 d-flex">
-                <a href="{{ route('bookings.show', $booking->id) }}"><img src="/storage/{{$booking->image}} " class="img-responsive rounded-start" alt="Hotel Image"></a>
-              </div>
-              <div class="col-sm-6 d-flex">
-                <div class="card-body">
-                  <a href="{{ route('bookings.show', $booking->id) }}">
-                    <h5 class="card-title">{{ $booking->name}}</h5>
-                  </a>
-                  <p class="card-text">Booking ID: {{ $booking->id}}</p>
-                  <p class="card-text">Country: {{ $booking->country }}</p>
-                  <p class="card-text">Check-in: {{ date('d-m-Y', strtotime($booking->checkInDate));  }}
-                  </p>
-                  <p class="card-text">{{ $booking->numNights }} nights</p>
-                  <p class="card-text">Payment: {{ $booking->payment_method }}</p>
-                  <p class="card-text"><small class="text-muted">Booking Created: {{ date('d-m-Y', strtotime($booking->created_at)); }}</small></p>
+            <div class="my-2">
+              <div class="row g-0">
+                <div class="col-sm-6 d-flex">
+                  <a href="{{ route('bookings.show', $booking->id) }}"><img src="/storage/{{$booking->image}} " class="img-responsive rounded-start" alt="Hotel Image"></a>
+                </div>
+                <div class="col-sm-6 d-flex">
+                  <div class="card-body">
+                    <a href="{{ route('bookings.show', $booking->id) }}">
+                      <h5 class="card-title">{{ $booking->name}}</h5>
+                    </a>
+                    <p class="card-text">Booking ID: {{ $booking->id}}</p>
+                    <p class="card-text">Country: {{ $booking->country }}</p>
+                    <p class="card-text">Check-in: {{ date('d-m-Y', strtotime($booking->checkInDate));  }}
+                    </p>
+                    <p class="card-text">{{ $booking->numNights }} nights</p>
+                    <p class="card-text">Payment: {{ $booking->payment_method }}</p>
+                    <p class="card-text"><small class="text-muted">Booking Created: {{ date('d-m-Y', strtotime($booking->created_at)); }}</small></p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <!-- End -->
-        </li>
+            <!-- End -->
+          </li>
+        </div>
         <!-- End -->
       </ul>
     </div>
