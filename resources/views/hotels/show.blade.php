087315b1aa0dc1d1d06f5235c7282e306445ee4f
kevster2017 <kevin.okane2017@gmail.com>
2023-12-22 13:31:56 +0000

    stars added to hotel show blade

--- resources/views/hotels/show.blade.php
+++ resources/views/hotels/show.blade.php
@@ -21,11 +21,14 @@
             <div class="col-md-8">
                 <div class="card-body">
                     <h2 class="card-title">{{ $hotel->name }}</h2>
-                    <p class="card-text">{{ $hotel->address }}</p>
-                    <p class="card-text">{{ $hotel->town }}</p>
-                    <p class="card-text">{{ $hotel->postCode }}</p>
-                    <p class="card-text">{{ $hotel->country }}</p>
-                    <p class="card-text"><small class="text-body-secondary">Hotel Added: {{ $hotel->created_at->diffForHumans() }}</small></p>
+                    @for ($i = 1; $i <= $hotel->stars; $i++)
+                        <span> <i class="fa-solid fa-star mb-3" style="color:gold"></i></span>
+                        @endfor
+                        <p class="card-text">{{ $hotel->address }}</p>
+                        <p class="card-text">{{ $hotel->town }}</p>
+                        <p class="card-text">{{ $hotel->postCode }}</p>
+                        <p class="card-text">{{ $hotel->country }}</p>
+                        <p class="card-text"><small class="text-body-secondary">Hotel Added: {{ $hotel->created_at->diffForHumans() }}</small></p>
                 </div>
             </div>
         </div>
